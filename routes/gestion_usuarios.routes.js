c1dfc71b0b9f1c7434364a557c4aad2b4b8675d2
JorgeForero02 <forero07andres@gmail.com>
2025-10-21 21:26:48 -0500

    Se agrego auditoria

--- routes/gestion_usuarios.routes.js
+++ routes/gestion_usuarios.routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const gestion_usuarios = require('../controllers/gestion_usuarios.controller');
 const { auth, isAdministrador, isGerenteOrAdmin } = require('../middlewares/auth');
 
-router.get('/users', auth, isAdministrador, gestion_usuarios.getAllUsersComplete);
-router.get('/users/:id', auth, gestion_usuarios.getUserComplete);
-router.put('/users/:id/profile', auth, gestion_usuarios.updateProfile);
-router.put('/users/:id/role-data', auth, gestion_usuarios.updateRoleData);
-router.put('/users/:id/company', auth, isGerenteOrAdmin, gestion_usuarios.changeCompany);
-router.put('/users/:id/password', auth, gestion_usuarios.changePassword);
-router.patch('/users/:id/status', auth, isAdministrador, gestion_usuarios.toggleUserStatus);
+router.get('/', auth, isAdministrador, gestion_usuarios.getAllUsersComplete);
+router.get('/:id', auth, gestion_usuarios.getUserComplete);
+router.put('/:id/profile', auth, gestion_usuarios.updateProfile);
+router.put('/:id/role-data', auth, gestion_usuarios.updateRoleData);
+router.put('/:id/company', auth, isGerenteOrAdmin, gestion_usuarios.changeCompany);
+router.put('/:id/password', auth, gestion_usuarios.changePassword);
+router.patch('/:id/status', auth, isAdministrador, gestion_usuarios.toggleUserStatus);
 
-router.post('/users', auth, isAdministrador, gestion_usuarios.createUser);
+router.post('/', auth, isAdministrador, gestion_usuarios.createUser);
 
 module.exports = router;
