bfd99630796bf5036878a43253d8dbd7073bab96
JorgeForero02 <forero07andres@gmail.com>
2025-11-19 14:51:20 -0500

    Correccion de errores notificaciones

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -220,7 +220,7 @@ class AuthController {
                 });
             }
 
-            EmailService.enviarBienvenidaOrganizador(correo, nombre, resultado.datos.empresa.nombre).catch(errorEmail => {
+            EmailService.enviarBienvenidaOrganizador(correo, nombre, resultado.datos.empresa.nombre, contraseña).catch(errorEmail => {
                 console.error('Error al enviar email de bienvenida al organizador:', errorEmail);
             });
 
